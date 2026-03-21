8ea43342bc4cabe166efd2cc9deef50534af7ae1
DjoAHP <djodjo.vely@gmail.com>
2026-03-21 10:44:10 +0100

    🚀 favicon et logo integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+// Filtre l'erreur parasite des extensions Chrome
+const originalError = console.error;
+console.error = (...args) => {
+  if (
+    typeof args[0] === "string" &&
+    args[0].includes("message channel closed before a response")
+  )
+    return;
+  originalError(...args);
+};
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
